bc853031c661980d00b87b8a9cd7a7720ad3cedb
Zed <zedcaoile@gmail.com>
2025-11-28 17:49:24 +0800

    update cohere initialization

--- backend/src/config/cohere.js
+++ backend/src/config/cohere.js
@@ -1,7 +1,7 @@
-import { CohereClient } from 'cohere-ai';
-import dotenv from 'dotenv';
+import { CohereClientV2 } from "cohere-ai";
+import dotenv from "dotenv";
 dotenv.config();
 
-export const cohere = new CohereClient({
-  apiKey: process.env.COHERE_API_KEY
+export const cohere = new CohereClientV2({
+  token: process.env.COHERE_API_KEY
 });
