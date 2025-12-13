c9f5727800a94f01b5da39bee452974cc6f55cb2
Zed <zedcaoile@gmail.com>
2025-12-13 18:41:48 +0800

    add onDone callback to TextTypewriter for signaling animation completion

--- frontend/src/pages/chat/TextTypewriter.jsx
+++ frontend/src/pages/chat/TextTypewriter.jsx
@@ -2,21 +2,25 @@ import React from 'react'
 /* eslint-disable-next-line no-unused-vars */
 import { motion } from 'motion/react'
 
-const TextTypewriter = ({ text }) => {
+const TextTypewriter = ({ onDone, text }) => {
   const characters = text.split('')
 
   return (
     <span>
-      {characters.map((char, i) => (
-        <motion.span
-          key={i}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.05, delay: i * 0.01 }}
-        >
-          {char}
-        </motion.span>
-      ))}
+      {characters.map((char, i) => {
+        const isLastChar = i === characters.length - 1
+        return (
+          <motion.span
+            key={i}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.05, delay: i * 0.01 }}
+            onAnimationComplete={isLastChar ? onDone : undefined}
+          >
+            {char}
+          </motion.span>
+        )
+      })}
     </span>
   )
 }
