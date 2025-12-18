9faaccfd58c5bf79450784a9c9f1a53f1722dba0
Zed <zedcaoile@gmail.com>
2025-12-18 18:23:56 +0800

    move Toaster component to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,24 @@ import { Routes, Route } from 'react-router'
 import ChatPage from './pages/chat/ChatPage'
 import NotFound from './pages/NotFound'
 import LoginPage from './pages/login/LoginPage'
+import { Toaster } from 'sonner'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/chat' element={<ChatPage />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+    <>
+      <Toaster
+        toastOptions={{
+          className: 'flex justify-center',
+        }}
+        position='top-center'
+        richColors
+      />
+      <Routes>
+        <Route path='/chat' element={<ChatPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </>
   )
 }
 
--- frontend/src/pages/chat/ChatPage.jsx
+++ frontend/src/pages/chat/ChatPage.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import Navbar from './Navbar'
 import ChatInterface from './ChatInterface'
-import { Toaster } from 'sonner'
 
 const ChatPage = () => {
   return (
     <div className='flex h-screen flex-col overflow-y-auto [scrollbar-gutter:stable]'>
-      <Toaster
-        toastOptions={{
-          className: 'flex justify-center',
-        }}
-        position='top-center'
-        richColors
-      />
       <Navbar />
       <ChatInterface />
     </div>
