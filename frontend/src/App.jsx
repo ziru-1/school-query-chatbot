6a7584a14dad6f3ef7e2127d90467c4c21757690
Zed <zedcaoile@gmail.com>
2025-12-18 12:31:17 +0800

    add LoginPage component for admins and integrate it into routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router'
 import ChatPage from './pages/chat/ChatPage'
 import NotFound from './pages/NotFound'
+import LoginPage from './pages/login/LoginPage'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/chat' element={<ChatPage />} />
+      <Route path='/login' element={<LoginPage />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
   )
---
+++ frontend/src/pages/login/LoginPage.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import Navbar from '../chat/Navbar'
+import logo from '@/assets/temp-logo.png'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email)
+    console.log(password)
+  }
+
+  return (
+    <div className='flex min-h-screen flex-col'>
+      <Navbar />
+      <main className='flex flex-1 items-center justify-center'>
+        <div className='mb-30 space-y-2 rounded-lg p-12 shadow-lg'>
+          <div className='flex justify-center'>
+            <img
+              src={logo}
+              alt='Logo'
+              className='h-10 w-10 rounded-2xl shadow'
+            />
+          </div>
+          <h1 className='text-center text-2xl font-medium'>
+            Log in to your account
+          </h1>
+          <p className='text-muted-foreground mb-5 text-center'>
+            Welcome back! Please enter your details
+          </p>
+          <form onSubmit={handleSubmit} className='space-y-4'>
+            <div className='space-y-2'>
+              <Label htmlFor='email'>Email</Label>
+              <Input
+                id='email'
+                type='email'
+                placeholder='Enter your email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className='space-y-2'>
+              <Label htmlFor='password'>Password</Label>
+              <Input
+                id='password'
+                type='password'
+                placeholder='••••••••••••'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button type='submit' className='mt-2 min-w-full'>
+                Sign in
+              </Button>
+            </div>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default LoginPage
