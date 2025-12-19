a7b1407b9446250d539bbac8f25a50e50d4711d0
Zed <zedcaoile@gmail.com>
2025-12-19 13:36:29 +0800

    extract login form into separate component and add signing in state

---
+++ frontend/src/pages/login/LoginForm.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import logo from '@/assets/temp-logo.png'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { toast } from 'sonner'
+import { supabase } from '@/lib/supabase'
+import { useNavigate } from 'react-router'
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isSigningIn, setIsSigningIn] = useState(false)
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setIsSigningIn(true)
+
+    if (!email.trim() || !password.trim()) return
+
+    try {
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      })
+
+      if (error) throw error
+
+      navigate('/admin')
+    } catch {
+      toast.error('Invalid email or password')
+    } finally {
+      setIsSigningIn(false)
+    }
+  }
+  return (
+    <div className='-translate-y-15 space-y-2 rounded-lg p-12 shadow-lg'>
+      <div className='flex justify-center'>
+        <img src={logo} alt='Logo' className='h-10 w-10 rounded-2xl shadow' />
+      </div>
+      <h1 className='text-center text-2xl font-medium'>
+        Log in to your account
+      </h1>
+      <p className='text-muted-foreground mb-5 text-center'>
+        Welcome back! Please enter your details
+      </p>
+      <form onSubmit={handleSubmit} className='space-y-4'>
+        <div className='space-y-2'>
+          <Label htmlFor='email'>Email</Label>
+          <Input
+            id='email'
+            type='email'
+            placeholder='Enter your email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className='space-y-2'>
+          <Label htmlFor='password'>Password</Label>
+          <Input
+            id='password'
+            type='password'
+            placeholder='Enter your password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type='submit'
+            disabled={isSigningIn}
+            className='mt-2 min-w-full'
+          >
+            {isSigningIn ? 'Signing in...' : 'Sign in'}
+          </Button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm
--- frontend/src/pages/login/LoginPage.jsx
+++ frontend/src/pages/login/LoginPage.jsx
@@ -1,82 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Navbar from '../chat/Navbar'
-import logo from '@/assets/temp-logo.png'
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { toast } from 'sonner'
-import { supabase } from '@/lib/supabase'
-import { useNavigate } from 'react-router'
+
+import LoginForm from './LoginForm'
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  const navigate = useNavigate()
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    if (!email.trim() || !password.trim()) return
-
-    try {
-      const { error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-      })
-
-      if (error) throw error
-
-      navigate('/admin')
-    } catch {
-      toast.error('Invalid email or password')
-    }
-  }
-
   return (
     <div className='flex min-h-screen flex-col'>
       <Navbar />
       <main className='flex flex-1 items-center justify-center'>
-        <div className='mb-30 space-y-2 rounded-lg p-12 shadow-lg'>
-          <div className='flex justify-center'>
-            <img
-              src={logo}
-              alt='Logo'
-              className='h-10 w-10 rounded-2xl shadow'
-            />
-          </div>
-          <h1 className='text-center text-2xl font-medium'>
-            Log in to your account
-          </h1>
-          <p className='text-muted-foreground mb-5 text-center'>
-            Welcome back! Please enter your details
-          </p>
-          <form onSubmit={handleSubmit} className='space-y-4'>
-            <div className='space-y-2'>
-              <Label htmlFor='email'>Email</Label>
-              <Input
-                id='email'
-                type='email'
-                placeholder='Enter your email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className='space-y-2'>
-              <Label htmlFor='password'>Password</Label>
-              <Input
-                id='password'
-                type='password'
-                placeholder='Enter your password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Button type='submit' className='mt-2 min-w-full'>
-                Sign in
-              </Button>
-            </div>
-          </form>
-        </div>
+        <LoginForm />
       </main>
     </div>
   )
