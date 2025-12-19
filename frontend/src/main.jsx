14a0f91d8d66265d47ab7dd300a8d276c8aa7eec
Zed <zedcaoile@gmail.com>
2025-12-19 13:29:48 +0800

    remove unused Routes and Route imports from main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter } from 'react-router'
 import App from './App'
 import './index.css'
 
